09951e521fa95cd3c1b65cf00ef72bd8985905b1
Prajwal Gaonkar <prajwal.gaonkar.x7@gmail.com>
2020-06-07 19:46:26 +0530

    now copies config files from .config directory

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 # syncs my dotFiles with the GitHub repository.
 
 # copy my updated dotFiles from my system to this repository.
+rm -rf .config
+mkdir .config
 cp -rf ~/.config/mpv ~/.config/ranger ~/.config/nvim ~/.config/user-dirs.dirs ~/.config/wal.png ./.config/
 cp -rf ~/scripts/ ~/.bashrc ~/.profile ~/.xinitrc .
 # add updated file, commit changes, and push changes.
