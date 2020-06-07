110952d4951feb699208d60eb27db8bdeba0cc86
Prajwal Gaonkar <prajwal.gaonkar.x7@gmail.com>
2020-06-07 19:21:48 +0530

    automated updates with this script

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
+# syncs my dotFiles with the GitHub repository.
+
+# copy my updated dotFiles from my system to this repository.
 cp -rf ~/scripts/ ~/.bashrc ~/.profile ~/.xinitrc .
+# add updated file, commit changes, and push changes.
 git add \*
 git commit
 git push
