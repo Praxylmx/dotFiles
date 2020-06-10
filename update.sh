7d4cd726da4d448fac64066f52325b5c42ab6641
Prajwal Gaonkar <prajwal.gaonkar.x7@gmail.com>
2020-06-10 18:46:22 +0530

    updated

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# syncs my dotFiles with the GitHub repository.
+#syncs my dotFiles with the GitHub repository.
 
-# copy my updated dotFiles from my system to this repository.
+#copy my updated dotFiles from my system to this repository.
 rm -rf .config
 mkdir .config
 cp -rf ~/.config/mpv ~/.config/ranger ~/.config/nvim ~/.config/user-dirs.dirs ~/.config/wal.png .config/
 cp -rf ~/scripts/ ~/.bashrc ~/.profile ~/.xinitrc .
-# add updated file, commit changes, and push changes.
+#add updated file, commit changes, and push changes.
 git add \*
 git commit
 git push
